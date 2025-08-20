eccab3e0c7d544dcb5c53727eece22f5ae084326
Tejas <tejas@mavenagency.co.za>
2025-08-20 17:34:22 +0530

    Admin Dashboard

--- layouts/admin_sidebar.php
+++ layouts/admin_sidebar.php
@@ -1,7 +1,7 @@
 <div class="sidebar bg-light p-3">
     <h5>Admin Panel</h5>
     <ul class="nav flex-column">
-        <li class="nav-item"><a href="<?= BASE_URL ?>/views/admin/dashboard/" class="nav-link">Dashboard</a></li>
+        <li class="nav-item"><a href="<?= BASE_URL ?>/admin/dashboard/" class="nav-link">Dashboard</a></li>
         <li class="nav-item"><a href="<?= BASE_URL ?>/views/admin/manage_users.php" class="nav-link">Manage Users</a></li>
         <li class="nav-item"><a href="<?= BASE_URL ?>/views/admin/manage_exercises.php" class="nav-link">Exercises</a></li>
         <li class="nav-item"><a href="<?= BASE_URL ?>/views/admin/manage_referrals.php" class="nav-link">Referral Sources</a></li>
