ec2a5b0daf5ff1da3f144ea88afc10c6b518f637
tejasmaven <34572285+tejasmaven@users.noreply.github.com>
2025-08-20 13:07:21 +0530

    Use dynamic base URL for admin links

--- layouts/admin_sidebar.php
+++ layouts/admin_sidebar.php
@@ -1,13 +1,13 @@
 <div class="sidebar bg-light p-3">
     <h5>Admin Panel</h5>
     <ul class="nav flex-column">
-        <li class="nav-item"><a href="/views/dashboard/admin_dashboard.php" class="nav-link">Dashboard</a></li>
-        <li class="nav-item"><a href="/views/admin/manage_users.php" class="nav-link">Manage Users</a></li>
-        <li class="nav-item"><a href="./views/admin/manage_exercises.php" class="nav-link">Exercises</a></li>
-        <li class="nav-item"><a href="/views/admin/manage_referrals.php" class="nav-link">Referral Sources</a></li>
-        <li class="nav-item"><a href="/views/admin/financial_reports.php" class="nav-link">Financial Reports</a></li>
-        <li class="nav-item"><a href="/views/admin/clinic_dashboards.php" class="nav-link">Clinic Dashboard</a></li>
-        <li class="nav-item"><a href="/views/admin/treatment_viewer.php" class="nav-link">All Treatments</a></li>
-        <li class="nav-item"><a href="../../views/shared/logout.php" class="nav-link">Logout</a></li>
+        <li class="nav-item"><a href="<?= BASE_URL ?>/views/dashboard/admin_dashboard.php" class="nav-link">Dashboard</a></li>
+        <li class="nav-item"><a href="<?= BASE_URL ?>/views/admin/manage_users.php" class="nav-link">Manage Users</a></li>
+        <li class="nav-item"><a href="<?= BASE_URL ?>/views/admin/manage_exercises.php" class="nav-link">Exercises</a></li>
+        <li class="nav-item"><a href="<?= BASE_URL ?>/views/admin/manage_referrals.php" class="nav-link">Referral Sources</a></li>
+        <li class="nav-item"><a href="<?= BASE_URL ?>/views/admin/financial_reports.php" class="nav-link">Financial Reports</a></li>
+        <li class="nav-item"><a href="<?= BASE_URL ?>/views/admin/clinic_dashboards.php" class="nav-link">Clinic Dashboard</a></li>
+        <li class="nav-item"><a href="<?= BASE_URL ?>/views/admin/treatment_viewer.php" class="nav-link">All Treatments</a></li>
+        <li class="nav-item"><a href="<?= BASE_URL ?>/views/shared/logout.php" class="nav-link">Logout</a></li>
     </ul>
 </div>
