a222b5fdb02b3bc08452d64a7843af7cd40541dd
tejasmaven <34572285+tejasmaven@users.noreply.github.com>
2025-08-21 11:34:35 +0530

    Filter sessions by episode

--- views/doctor/save_treatment.php
+++ views/doctor/save_treatment.php
@@ -7,9 +7,13 @@ requireRole('Doctor');
 $treatmentController = new TreatmentController($pdo);
 
 // Validate essential fields
-if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['patient_id'], $_POST['session_date'], $_POST['doctor_id'])) {
+if (
+    $_SERVER['REQUEST_METHOD'] === 'POST' &&
+    isset($_POST['patient_id'], $_POST['episode_id'], $_POST['session_date'], $_POST['doctor_id'])
+) {
     $data = [
         'patient_id' => $_POST['patient_id'],
+        'episode_id' => $_POST['episode_id'],
         'session_date' => $_POST['session_date'],
         'doctor_id' => $_POST['doctor_id'],
         'remarks' => $_POST['remarks'] ?? '',
